04bd19b6d713c93af29b36743032e76d5316ef49
prince13207 <prince0078956@gmail.com>
2018-07-06 08:26:09 +0530

    Added morgan for logging

--- nodePractice/index.js
+++ nodePractice/index.js
@@ -1,12 +1,16 @@
 const express= require('express');
 const http = require('http');
+const morgan = require('morgan');
 
 const app=express();
 
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname+'/public'));
 
 app.use((req,res,next)=>{
 
-console.log(req.header());
+
 res.end('<html><body><h1>This is Express Server</h1></body></html>');
 });
 

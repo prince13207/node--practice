fa746f28e201c01a0be3b39593b13fe97c687299
prince13207 <prince0078956@gmail.com>
2018-07-11 08:59:39 +0530

    Assignment 1

--- nodePractice/index.js
+++ nodePractice/index.js
@@ -3,14 +3,14 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser =require('body-parser');
 const dishRouter = require('./routes/dishRouter');
-
+const promoRouter= require('./routes/promoRouter');
+const leaderRouter=require('./routes/leaderRouter');
 const app=express();
 
 app.use(morgan('dev'));
 app.use('/dishes',dishRouter);
-app.use('/dishes/:dishId',dishRouter);
-
-
+app.use('/leaders',leaderRouter);
+app.use('/promotions',promoRouter);
 app.use(express.static(__dirname +'/public'));
 
 
--- nodePractice/routes/dishRouter.js
+++ nodePractice/routes/dishRouter.js
@@ -16,24 +16,20 @@ dishRouter.route('/')
 })
 .get((req,res,next)=>{
  
-    res.end('Sending all dishes');
+    res.end('Will send all the dishes to you!');
 
 })
 .post((req,res,next)=>{
   
-    res.end('Adding dishes '+req.body.name+' description '+req.body.description);
-
+    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
 })
 .put((req,res,next)=>{
-    console.log(req.params.dishId);
-    res.statusCode=403;
-    res.end('This operation not supported');
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dishes');
 
 })
 .delete((req,res,next)=>{
-    console.log(req.params.dishId);
     res.end('Deleting all dishes');
-
 });
 
 
@@ -45,24 +41,24 @@ dishRouter.route('/:dishId')
 })
 .get((req,res,next)=>{
 
-    res.end('Sending dish '+req.params.dishId);
+    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
 
 })
 .post((req,res,next)=>{
-    res.statusCode=403;
-    res.end('Operation not suported for dishes/'+req.params.dishId);
+    res.statusCode = 403;
+  res.end('POST operation not supported on /dishes/'+ req.params.dishId);
 
 })
 .put((req,res,next)=>{
 
-    res.write('Updating dish for '+req.params.dishId+'\n');
-
-    res.end('Updating dish item for '+req.body.name+' with details '+req.body.description);
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name + 
+          ' with details: ' + req.body.description);
 
 })
 .delete((req,res,next)=>{
 
-    res.end('Deleting dish '+req.params.dishId);
+    res.end('Deleting dish: ' + req.params.dishId);
 
 });
 
